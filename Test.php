89f2bbb36ddeab74d3bf99560df471b7cdeedb04
suliang <sl@nongline.com>
2017-12-29 15:35:06 +0800

    Initialize the project

---
+++ Test.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: su
+ * Date: 2017/12/29
+ * Time: 15:29
+ */
+require 'vendor/autoload.php';
+
+//use MiniUpload\MyUpload as MyUpload;
+use Trhui\Tpam as Tpam;
+
+$miniUpload = new Tpam();
+$miniUpload->test();
---
+++ src/Tpam.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: su
+ * Date: 2017/12/29
+ * Time: 15:21
+ */
+
+namespace Trhui;
+
+class Tpam
+{
+    public function test()
+    {
+        echo 'test';
+    }
+}
