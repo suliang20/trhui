154672dade8aa51d0a23e4474905853e99096482
suliang20 <suliang@163.com>
2018-01-09 21:51:43 +0800

    新增回调示例页面

---
+++ sample/notify.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: su
+ * Date: 2018/1/4
+ * Time: 18:08
+ */
+require_once('./config.php');
+require '../vendor/autoload.php';
+
+$result = new \trhui\Results();
+$result->tpamPublicKeyPath = PUBLIC_KEY_PATH;
+$res = $result->handle($_POST);
+var_dump($res);
+if (!$res) {
+    var_dump($result->errors);
+    exit;
+}
+
+
