7e2f5a74c42e71999ff988cedf3036c8bb42586e
suliang20 <suliang@163.com>
2018-01-09 21:19:46 +0800

    添加测试首页

---
+++ sample/index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+    <title>招商银行清算通测试</title>
+    <script type="text/javascript" src="jquery-3.2.1.min.js"></script>
+
+</head>
+
+<body>
+<p><a href="register.php">注册</a></p>
+<p><a href="pay.php">支付</a></p>
+</body>
+</html>
